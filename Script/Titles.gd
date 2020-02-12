4e990b4067d04e40e254fe9b657fcb7d38bdf8be
Ignacio Peralta <44816790+Kisotyvevo@users.noreply.github.com>
2020-02-12 00:41:40 -0300

    sdasdsd

--- Script/Titles.gd
+++ Script/Titles.gd
@@ -1,16 +1,27 @@
 extends Node
+var Mundo = "res://Escenas/Mundo.tscn"
 
 func _ready():
 	pass
 
 func _on_Inicio_pressed():
-	$"title-Inicio".visible = true
+	$"Loading".visible = true
+	$Inicio.visible = false
 	$Creditos.visible = false
+	$MarginContainer/VBoxContainer/TextureRect.visible = false
+	$Nuces/Sprite.visible = false
+	$Nuces/Sprite2.visible = false
+	$Nuces/Sprite3.visible = false
+	$Nuces/Sprite4.visible = false
+	$Nuces/Sprite5.visible = false
+	$Nuces/Sprite6.visible = false
+	$Timer.start()
+	load(Mundo)
 	
 
 func _on_ButtonInicio_pressed():
-	get_tree().change_scene("res://Escenas/Mundo.tscn")
 	
+	pass
 
 func _on_Creditos_pressed():
 	$Creditos2.visible = true
@@ -26,3 +37,8 @@ func _physics_process(delta):
 		get_tree().quit()
 	if Input.is_action_pressed("ui_up"):
 		get_tree().change_scene("res://Escenas/Mundo.tscn")
+
+
+func _on_Timer_timeout():
+	get_tree().change_scene("res://Escenas/Mundo.tscn")
+	pass # Replace with function body.
