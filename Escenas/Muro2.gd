1b035017dcac1b1962aa45e5f68fa1478fc0f260
franbaumann <70665473+franbaumann@users.noreply.github.com>
2020-09-05 22:13:06 -0300

    Nivel 2 añadido
    Nivel dos hecho en base a la escena del nivel uno. La escena principal es la del nivel 2 para poder testearlo bien. Se puede cambiar cualquier cosa.

---
+++ Escenas/Muro2.gd
@@ -0,0 +1,16 @@
+extends StaticBody2D
+
+func _ready():
+	pass
+
+
+func _on_Palanca2_body_entered(body):
+	if "Player" in body.name:
+		$"../Palanca3/Palanca3 anim".play("Off")
+		$".".visible = false
+		$CollisionShape2D.disabled = true
+		$Timer.start()
+
+
+func _on_Timer_timeout():
+	queue_free()
